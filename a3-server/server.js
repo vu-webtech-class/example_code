66d308247b880bf2594395bb2468b114cc41854d
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@vu.nl>
2025-01-03 13:21:05 +0100

    Updated to travel theme

--- a3-server/server.js
+++ a3-server/server.js
@@ -24,7 +24,7 @@
 // It will be automatically re-created and filled with one example item.
 
 const sqlite = require("sqlite3").verbose();
-let db = my_database("./media.db");
+let db = my_database("./travel.db");
 
 // ###############################################################################
 // The database should be OK by now. Let's setup the Web server so we can start
@@ -63,8 +63,8 @@ app.get("/hello", function (req, res) {
 app.get("/db-example", function (req, res) {
   // Example SQL statement to select the name of all products from a specific brand
   db.all(
-    `SELECT * FROM media WHERE name=?`,
-    ["Celeste"],
+    `SELECT * FROM travel WHERE place=?`,
+    ["Amsterdam"],
     function (err, rows) {
       // TODO: add code that checks for errors so you know what went wrong if anything went wrong
       // TODO: set the appropriate HTTP response headers and HTTP response codes here.
@@ -76,7 +76,7 @@ app.get("/db-example", function (req, res) {
 });
 
 app.post("/post-example", function (req, res) {
-  // This is just to check if there is any data posted in the body of the HTTP request:
+  // This is just so you may check if there is any data posted in the body of the HTTP request:
   console.log(req.body);
   return res.json(req.body);
 });
@@ -97,44 +97,31 @@ function my_database(filename) {
     if (err) {
       console.error(err.message);
     }
-    console.log("Connected to the media database.");
+    console.log("Connected to the travel database.");
   });
-  // Create our media table if it does not exist already:
+  // Create our travel table if it does not exist already:
   db.serialize(() => {
     db.run(`
-        	CREATE TABLE IF NOT EXISTS media
+        	CREATE TABLE IF NOT EXISTS travel
         	 (
                     id INTEGER PRIMARY KEY,
-                    name CHAR(100) NOT NULL,
-                    year CHAR(100) NOT NULL,
-                    genre CHAR(256) NOT NULL,
-                    poster char(2048) NOT NULL,
+                    place CHAR(100) NOT NULL,
+                    visited CHAR(100) NOT NULL,
+                    keywords CHAR(256) NOT NULL,
+                    image char(2048) NOT NULL,
                     description CHAR(1024) NOT NULL
 		 )
 		`);
-    db.all(`select count(*) as count from media`, function (err, result) {
+    db.all(`select count(*) as count from travel`, function (err, result) {
       if (result[0].count == 0) {
-        db.run(
-          `INSERT INTO media (name, year, genre, poster, description) VALUES (?, ?, ?, ?, ?)`,
-          [
-            "Arcane",
-            "2021",
-            "animation, action, adventure, tv-show",
-            "https://www.nerdpool.it/wp-content/uploads/2021/11/poster-arcane.jpg",
-            "Set in Utopian Piltover and the oppressed underground of Zaun, the story follows the origins of two iconic League Of Legends champions and the power that will tear them apart.",
-            ]
-        );
-        db.run(
-          `INSERT INTO media (name, year, genre, poster, description) VALUES (?, ?, ?, ?, ?)`,
-          [
-            "Celeste",
-            "2018",
-            "platformer, video-game",
-            "https://upload.wikimedia.org/wikipedia/commons/0/0f/Celeste_box_art_full.png",
-            "Celeste is a critically acclaimed two-dimensional platform game developed by Maddy Makes Games. The player controls Madeline, a young woman who sets out to climb Celeste Mountain. The game features tight controls, challenging levels, and a touching story about overcoming personal demons.",
-          ]
-        );
-        console.log("Inserted dummy photo entry into empty database");
+        db.run(`INSERT INTO travel (image, place, visited, keywords, description) VALUES (?, ?, ?, ?, ?)`, [
+          "https://i1.pickpik.com/photos/294/94/486/urban-bike-a82f94fdeed9cfbfbd1fd3723e97d2db.jpg",
+          "Amsterdam",
+          "4",
+          "canals, coffee shops, tulips",
+          "Amsterdam was founded at the mouth of the Amstel River, which was dammed to control flooding. Originally a small fishing village in the 12th century, Amsterdam became a major world port during the Dutch Golden Age of the 17th century, when the Netherlands was an economic powerhouse.",
+      ]);
+        console.log("Inserted dummy example entry into empty database");
       } else {
         console.log(
           "Database already contains",
